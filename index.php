9c0fb37866f5f31b3d1de4bacf830a67502d62cd
dsnid21 <dms0082@mix.wvu.edu>
2021-02-02 20:21:22 -0500

    Lab 2 Revamped

--- index.php
+++ index.php
@@ -3,48 +3,56 @@
 
     <div class="about">
         <h1>About Us</h1>
-        <p style="color: white">Who is David Snider?</p>
+        <p style="color: white">Movie Reviews by the most prominent movies directors.</p>
+        <button type="button">Back to Home Page</button>
     </div>
     <div class="row">
         <div class="column">
             <div class="card">
-                <img src="images/snider_david.jpeg">
+                <img src="images/tarantino.jpeg">
                 <div class="container">
-                    <h2>Author</h2>
-                    <p>David Snider</p>
-                    <p>Email: dms0082@mix.wvu.edu</p>
+                    <h2>Reviewer</h2>
+                    <p>Quinten Tarantino</p>
+                    <p>Email: tarantino@gmail.com</p>
                 </div>
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="images/snider.jpeg">
+                <img src="images/scorsese.jpeg">
                 <div class="container">
-                    <h2>Student</h2>
-                    <p>David Snider</p>
-                    <p>West Virginia University</p>
+                    <h2>Reviewer</h2>
+                    <p>Martin Scorsese</p>
+                    <p>Email: scorsese@gmail.com</p>
                 </div>
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="images/swimming.jpeg">
+                <img src="images/ficher.jpeg">
                 <div class="container">
-                    <h2>Athlete</h2>
-                    <p>David Snider</p>
-                    <p>WVU Men's Swimming</p>
+                    <h2>Reviewer</h2>
+                    <p>David Fincher</p>
+                    <p>Email: fincher@gmail.com</p>
                 </div>
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="images/finish.jpeg">
+                <img src="images/spielberg.jpeg">
                 <div class="container">
-                    <h2>Gentleman</h2>
-                    <p>David Snider</p>
-                    <p>A right fellow chap.</p>
+                    <h2>Reviewer</h2>
+                    <p>Steven Spielberg</p>
+                    <p>Email: et@gmail.com</p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="description">
+        <h2>The 4 Horsemen</h2>
+        <p>We are 4 movie directors who have topped charts in years past with our films.  We 
+            designed this team to review student made films in hope to shed a light on 
+            the next generation of film makers.
+        </p>
     </div>
 </body>
