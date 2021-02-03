96c4ce19c2d24bbc47212a0b26b8a1db3aedb6a8
dsnid21 <dms0082@mix.wvu.edu>
2021-02-02 19:44:19 -0500

    Lab 2 About Page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p style="color: white">Who is David Snider?</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/snider_david.jpeg">
+                <div class="container">
+                    <h2>Author</h2>
+                    <p>David Snider</p>
+                    <p>Email: dms0082@mix.wvu.edu</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/snider.jpeg">
+                <div class="container">
+                    <h2>Student</h2>
+                    <p>David Snider</p>
+                    <p>West Virginia University</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/swimming.jpeg">
+                <div class="container">
+                    <h2>Athlete</h2>
+                    <p>David Snider</p>
+                    <p>WVU Men's Swimming</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/finish.jpeg">
+                <div class="container">
+                    <h2>Gentleman</h2>
+                    <p>David Snider</p>
+                    <p>A right fellow chap.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
